b60052c50b9d576aca8831b5c1f1129e82d57c7f
Paweł Gągorowski <pawelgagorowski026@gmail.com>
2022-12-10 00:52:01 +0100

    B2-commit-1

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ console.log("test2");
 console.log("test3");
 console.log("test4");
 console.log("test5");
+<<<<<<< HEAD
 console.log("test6");
 console.log("test7");
 console.log("test8");
@@ -11,3 +12,6 @@ console.log("test1")
 console.log("test2");
 console.log("test3");
 console.log("test4");
+=======
+console.log("test6");
+>>>>>>> 236e3eb (B2-commit-1)
